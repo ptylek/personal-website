bc6c83af11bd1bf7b9ab062f3390a2760829c4a7
Paweł Tylek <pawel.tylek@icloud.com>
2024-03-02 13:57:16 +0100

    feat: Update Nuxt to 3.10.3 along with dependencies

--- app.config.ts
+++ app.config.ts
@@ -1,6 +1,15 @@
 export default defineAppConfig({
+  /**
+   * Nuxt UI
+   * https://ui.nuxt.com/getting-started/theming
+   */
   ui: {
     primary: 'indigo',
     gray: 'zinc'
-  }
+  },
+  /**
+   * TODO: Remove this when the issue listed below is fixed.
+  * https://github.com/nuxt/ui/issues/1043
+  */
+  nuxtIcon: {}
 })
